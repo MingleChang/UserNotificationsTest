8a2f2d05113ff7af91de1a1ec67594c5ce287c0f
Mingle Chang <mingle890805@gmail.com>
2016-10-30 23:17:17 +0800

    not

--- NotificationService/NotificationService.m
+++ NotificationService/NotificationService.m
@@ -26,8 +26,18 @@
     self.bestAttemptContent.subtitle = [NSString stringWithFormat:@"%@ [NotificationService]", self.bestAttemptContent.subtitle];
     self.bestAttemptContent.body = [NSString stringWithFormat:@"%@ [NotificationService]", self.bestAttemptContent.body];
     
-    
-    self.contentHandler(self.bestAttemptContent);
+    NSDictionary *lApsDic = self.bestAttemptContent.userInfo[@"aps"];
+    NSString *lImageUrl=lApsDic[@"image"];
+    if (lImageUrl.length>0) {
+        [self loadAttachmentForUrlString:lImageUrl withType:@"png" completionHandle:^(UNNotificationAttachment *attach) {
+            if (attach) {
+                self.bestAttemptContent.attachments = [NSArray arrayWithObject:attach];
+            }
+            self.contentHandler(self.bestAttemptContent);
+        }];
+    }else{
+        self.contentHandler(self.bestAttemptContent);
+    }
 }
 
 - (void)serviceExtensionTimeWillExpire {
@@ -36,6 +46,29 @@
     self.contentHandler(self.bestAttemptContent);
 }
 
-#pragma mark -
+#pragma mark - Private
+- (void)loadAttachmentForUrlString:(NSString *)urlStr withType:(NSString *)type completionHandle:(void(^)(UNNotificationAttachment *attach))completionHandler{
+    __block UNNotificationAttachment *attachment = nil;
+    NSURL *attachmentURL = [NSURL URLWithString:urlStr];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    NSURLSessionDownloadTask *lTask=[session downloadTaskWithURL:attachmentURL completionHandler:^(NSURL *temporaryFileLocation, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"Error:%@", error.localizedDescription);
+        } else {
+            NSFileManager *fileManager = [NSFileManager defaultManager];
+            NSURL *localURL = [NSURL fileURLWithPath:[temporaryFileLocation.path stringByAppendingPathExtension:type]];
+            [fileManager moveItemAtURL:temporaryFileLocation toURL:localURL error:&error];
+            
+            NSError *attachmentError = nil;
+            attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:localURL options:nil error:&attachmentError];
+            if (attachmentError) {
+                NSLog(@"Error:%@", attachmentError.localizedDescription);
+            }
+        }
+        completionHandler(attachment);
+    }];
+    [lTask resume];
+}
 
 @end
